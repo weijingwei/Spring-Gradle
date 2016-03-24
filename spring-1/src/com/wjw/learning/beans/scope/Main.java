7ce13e1c5688006e21049147882cdcc269083d65
weijingwei <weijingwei@weijingwei-PC>
2016-03-24 23:28:28 +0800

    bean的作用域。singleton与prototype

---
+++ spring-1/src/com/wjw/learning/beans/scope/Main.java
@@ -0,0 +1,18 @@
+package com.wjw.learning.beans.scope;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.wjw.learning.beans.autowire.Car;
+
+public class Main {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-scope.xml");
+		Car car = (Car) ctx.getBean("car");
+		Car car2 = (Car) ctx.getBean("car");
+		System.out.println(car == car2);
+	}
+
+}
